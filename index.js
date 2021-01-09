bdcf2ccdb04015c3fe5de79d849e23883f506332
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-09 16:06:25 -0500

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,24 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+
+function displayMessage(str) {
+  document.getElementById("greeting").innerText = str;
+}
+
+
+
 /* Write your implementation of greet() */
+function greet(time) {
+  let fullNum = time.split(":").join("");
+  let parsed = parseInt(fullNum, 10);
+
+  if (parsed < 1200) {
+    return "Good Morning";
+  } else if (parsed > 1200 && parsed < 1500) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  }
+}
 /* Write your implementation of displayMessage() */
