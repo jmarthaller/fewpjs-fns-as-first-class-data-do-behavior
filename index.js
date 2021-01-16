b64062abb43bbf6d0134e69aab137ec29db2df1a
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-16 09:36:17 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* Given Code, don't edit */
+// /* Given Code, don't edit */
 
 document.querySelector("button").addEventListener("click", handleClick);
 
@@ -7,26 +7,82 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
-/* End Given Code, don't edit above here...*/
+// /* End Given Code, don't edit above here...*/
+const greetingHead = document.querySelector('#greeting')
 
-
-function displayMessage(str) {
-  document.getElementById("greeting").innerText = str;
-}
-
-
-
-/* Write your implementation of greet() */
 function greet(time) {
-  let fullNum = time.split(":").join("");
-  let parsed = parseInt(fullNum, 10);
-
-  if (parsed < 1200) {
-    return "Good Morning";
-  } else if (parsed > 1200 && parsed < 1500) {
-    return "Good Afternoon";
+  let timeToArray = time.split(":")
+  let parsedTime = Number(timeToArray[0])
+  if (parsedTime < 12) {
+    return "Good Morning"
+  } else if (parsedTime >= 12 && parsedTime <= 17) {
+    return "Good Afternoon"
   } else {
-    return "Good Evening";
+    return "Good Evening"
   }
 }
-/* Write your implementation of displayMessage() */
+
+
+function displayMessage(message) {
+  //const newHead = document.createElement("h1")
+  const greetingHead = document.querySelector('#greeting')
+  greetingHead.innerText = message
+  // greetingHead.append(newHead)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function displayMessage(str) {
+//   document.getElementById("greeting").innerText = str;
+// }
+
+
+
+// /* Write your implementation of greet() */
+// function greet(time) {
+//   let fullNum = time.split(":").join("");
+//   let parsed = parseInt(fullNum, 10);
+
+//   if (parsed < 1200) {
+//     return "Good Morning";
+//   } else if (parsed > 1200 && parsed < 1500) {
+//     return "Good Afternoon";
+//   } else {
+//     return "Good Evening";
+//   }
+// }
+// /* Write your implementation of displayMessage() */
